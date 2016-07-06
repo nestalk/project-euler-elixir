bec4d6e3bb09f213531d1a4cfbe8115a20b10ab5
Neil Stalker <nestalk@gmail.com>
2016-07-06 15:09:19 +0100

    Problem 2

--- lib/euler.ex
+++ lib/euler.ex
@@ -1,8 +1,10 @@
 defmodule Euler do
 
     def main(argv) do
-        IO.puts('Problem 1')
-        result = Euler.Problem1.run(1000)
-        IO.puts(result)
+        IO.puts("Problem 1")
+        IO.puts(Euler.Problem1.run(1000))
+
+        IO.puts("\nProblem 2")
+        IO.puts(Euler.Problem2.run(4000000))
     end
 end
